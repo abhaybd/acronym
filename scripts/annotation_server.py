dac4e9b8475e6b205ac57f806b4a8338398f01ac
Abhay D <a.deshpande012@gmail.com>
2025-01-24 18:43:47 -0800

    Load annotations on startup, remove malformed meshes

--- scripts/annotation_server.py
+++ scripts/annotation_server.py
@@ -23,6 +23,19 @@ for fn in os.listdir(f"data/grasps"):
         annotation_counts[category][obj_id] = set()
         malformed_counts[category][obj_id] = set()
 
+if os.path.isfile("annotations.jsonl"):
+    with open("annotations.jsonl", "r") as f:
+        for line in f:
+            annotation = Annotation.model_validate_json(line)
+            annotation_counts[annotation.obj.object_category][annotation.obj.object_id].add(annotation.grasp_id)
+if os.path.isfile("malformed.jsonl"):
+    with open("malformed.jsonl", "r") as f:
+        for line in f:
+            annotation = MalformedAnnotation.model_validate_json(line)
+            malformed_counts[annotation.obj.object_category][annotation.obj.object_id].add(annotation.user_id)
+            if annotation.obj.object_id in annotation_counts[annotation.obj.object_category]:
+                del annotation_counts[annotation.obj.object_category][annotation.obj.object_id]
+
 def num_annotations_category(category: str):
     return sum(map(len, annotation_counts[category].values()))
 
@@ -57,12 +70,10 @@ class MalformedMeshSubmission(BaseModel):
 @app.post("/api/get-object-grasp", response_model=MeshResponse)
 async def get_object_grasp():
     category = "Pan"
-    category = choose_from_least(CATEGORIES, num_annotations_category)
+    category = choose_from_least(annotation_counts.keys(), num_annotations_category)
     obj_id = choose_from_least(annotation_counts[category], key=lambda oid: len(annotation_counts[category][oid]))
 
     object_mesh: trimesh.Trimesh = load_mesh(f"data/grasps/{category}_{obj_id}.h5", "data")
-    print("Loaded from:", f"data/grasps/{category}_{obj_id}.h5")
-
     T, success = load_grasps(f"data/grasps/{category}_{obj_id}.h5")
     successful_grasp_ids = np.argwhere(success == 1).flatten()
     grasp_id = np.random.choice(successful_grasp_ids)
@@ -88,7 +99,7 @@ async def get_object_grasp():
 @app.post("/api/submit-annotation")
 async def submit_annotation(request: AnnotationSubmission, user_id: str = Cookie(...)):
     total_annotations = sum(map(num_annotations_category, annotation_counts.keys()))
-    print(f"User {user_id} annotated: {request.object_category}_{request.object_id}, grasp {request.grasp_id}. Total annotations: {total_annotations}")
+    print(f"User {user_id} annotated: {request.object_category}_{request.object_id}, grasp {request.grasp_id}. Total annotations: {total_annotations+1}")
     category = request.object_category
     obj_id = request.object_id
     grasp_id = request.grasp_id
@@ -110,5 +121,7 @@ async def malformed_mesh(request: MalformedMeshSubmission, user_id: str = Cookie
         obj=Object(object_category=request.object_category, object_id=request.object_id),
         user_id=user_id
     )
+    if request.object_id in annotation_counts[request.object_category]:
+        del annotation_counts[request.object_category][request.object_id]
     with open("malformed.jsonl", "a+") as f:
         f.write(f"{annotation.model_dump_json()}\n")
