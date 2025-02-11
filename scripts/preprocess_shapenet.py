fe866285c1e048a9a68f5ff36a9519dec32c6791
Abhay D <a.deshpande012@gmail.com>
2025-02-10 17:49:52 -0800

    Export annotation skeleton when preprocessing

--- scripts/preprocess_shapenet.py
+++ scripts/preprocess_shapenet.py
@@ -1,8 +1,8 @@
 import argparse
-import csv
 import os
 import shutil
 import re
+import pickle
 
 import h5py
 import numpy as np
@@ -17,8 +17,10 @@ def get_args():
     parser.add_argument("--n-grasps", type=int, default=16)
     return parser.parse_args()
 
+
 def rot_distance(rot_deltas: np.ndarray):
     return R.from_matrix(rot_deltas).magnitude()
+
 
 def grasp_dist(grasp: np.ndarray, grasps2: np.ndarray):
     assert grasp.ndim == 2
@@ -66,8 +68,12 @@ def main():
     os.makedirs(output_mesh_dir, exist_ok=True)
     os.makedirs(output_grasp_dir, exist_ok=True)
 
+    # maps (category, object_id, grasp_id) -> whether the grasp is annotated
+    annotation_skeleton: dict[str, dict[str, dict[int, bool]]] = {}
+
     for grasp_filename in tqdm(os.listdir(args.grasps_root)):
-        category = grasp_filename.split("_", 1)[0]
+        category, obj_id = grasp_filename.split("_", 1)
+        obj_id = obj_id[:-len(".h5")]
         mesh_src_dir = os.path.join(args.shapenet_root, "models-OBJ", "models")
         mesh_dst_dir = os.path.join(output_mesh_dir, category)
         os.makedirs(mesh_dst_dir, exist_ok=True)
@@ -79,19 +85,23 @@ def main():
         with h5py.File(os.path.join(output_grasp_dir, grasp_filename), "r+") as f:
             _, c, mesh_fn = f["object/file"][()].decode("utf-8").split("/")
             assert c == category
-            obj_id = mesh_fn[:-len(".obj")]
+            mesh_id = mesh_fn[:-len(".obj")]
             grasps = np.array(f["grasps/transforms"])
             successes = np.array(f["grasps/qualities/flex/object_in_gripper"])
             sampled_grasp_idxs = subsample_grasps(successes, grasps, args.n_grasps)
             f["grasps/sampled_idxs"] = sampled_grasp_idxs
+
+            if category not in annotation_skeleton:
+                annotation_skeleton[category] = {}
+            annotation_skeleton[category][obj_id] = {i.item(): False for i in sampled_grasp_idxs}
         
         shutil.copy2(
-            os.path.join(mesh_src_dir, f"{obj_id}.obj"),
-            os.path.join(mesh_dst_dir, f"{obj_id}.obj")
+            os.path.join(mesh_src_dir, f"{mesh_id}.obj"),
+            os.path.join(mesh_dst_dir, f"{mesh_id}.obj")
         )
 
         texture_files = set()
-        with open(os.path.join(mesh_src_dir, f"{obj_id}.mtl"), "r") as mtl_f:
+        with open(os.path.join(mesh_src_dir, f"{mesh_id}.mtl"), "r") as mtl_f:
             mtl_lines = []
             for line in mtl_f:
                 line = line.strip()
@@ -102,7 +112,7 @@ def main():
                     mtl_lines.append(line)
                 else:
                     mtl_lines.append(line)
-        with open(os.path.join(mesh_dst_dir, f"{obj_id}.mtl"), "w") as mtl_f:
+        with open(os.path.join(mesh_dst_dir, f"{mesh_id}.mtl"), "w") as mtl_f:
             mtl_f.write("\n".join(mtl_lines))
 
         for texture_file in texture_files:
@@ -110,6 +120,9 @@ def main():
                 os.path.join(args.shapenet_root, "models-textures", "textures", texture_file),
                 os.path.join(mesh_dst_dir, texture_file)
             )
+
+    with open(os.path.join(args.output_dir, "annotation_skeleton.pkl"), "wb") as f:
+        pickle.dump(annotation_skeleton, f)
 
 if __name__ == "__main__":
     main()
